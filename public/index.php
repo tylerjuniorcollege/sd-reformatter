7932ea363cf9a1a3685c81e4adc8db1b8cae4671
Duane Jeffers <duane@duanejeffers.com>
2014-08-20 09:53:25 -0500

    Adding composer.json and main application code

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	 * SD-Formatter (maybe renamed ... who knows)
+	 *
+	 * (c) Copyright 2014 Tyler Junior College
+	 * See LICENSE file for License details
+	 *
+	 * Purpose: A simple mini web application to make sure HTML is formatted for the SoftDocs system.
+	 * 			Uses Slim Framework and Idiorm.
+	 * Requirements: PHP 5.3+ and SQLite for simple user/database stores.
+	 **/
+
+	include 'vendor/autoload.php';
+
+	$app = new \Slim\Slim();
