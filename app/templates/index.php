0af4017352a4a05b6c519979bdd2cc923ed842eb
Duane Jeffers <duane@duanejeffers.com>
2014-08-20 16:42:42 -0500

    Quick Index created.

---
+++ app/templates/index.php
@@ -0,0 +1,33 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 well">
+			<p>Welcome to the SD HTML Formatter. This application is used to help you format your source files for inclusion in to SoftDocs.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<h3 class="panel-title">Upload HTML Document</h3>
+				</div>
+				<div class="panel-body">
+					<form class="form-inline" method="post" action="/html">
+						
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="panel panel-success">
+				<div class="panel-heading">
+					<h3 class="panel-title">Grab Form from URL</h3>
+				</div>
+				<div class="panel-body">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
