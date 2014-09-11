20a0afbf69b0d9bd7483f2e0a5a16f20182b389b
Duane Jeffers <duane@duanejeffers.com>
2014-09-11 16:42:48 -0500

    More parsing updates.

--- app/src/Parser/HTML.php
+++ app/src/Parser/HTML.php
@@ -14,9 +14,10 @@ namespace TJC\Parser;
 
 class HTML
 {
-	const IGNORE_NBSP = 'nbsp';
-	const IGNORE_UNIQUE = 'unique';
-	const IGNORE_READONLY = 'readonly';
+	const NBSP = 'nbsp';
+	const UNIQUEINPUT = 'uniqueinput';
+	const READONLY = 'readonly';
+	const EMPTYTAGS = 'emptytags';
 	protected $document;
 	protected $altered_document;
 	protected $doc_object;
@@ -54,7 +55,12 @@ class HTML
 
 	// This is only done near the end of the file generation. Before saving the final document.
 	public function replace_readonly() {
-		
+		$count = 0;
+		foreach($this->doc_object->find('input[readonly]') as $input) {
+			$input->readonly = 'readonly';
+			$count++;
+		}
+		return $count;
 	}
 
 	// Parse is the simple function that runs all the automation cleanup code. 
@@ -65,8 +71,8 @@ class HTML
 		$counters = array();
 		$results = array();
 
-		if(!in_array(self::IGNORE_NBSP, $args)) {
-			$counters['nbsp'] = $this->replace_nbsp();
+		if(!in_array(self::NBSP, $args)) {
+			$counters[self::NBSP] = $this->replace_nbsp();
 		}
 
 		// Now we put the document in the parsing object.
@@ -74,16 +80,29 @@ class HTML
 		$this->doc_object->load($this->altered_document);
 
 		// Check for unique names foreach input element.
-		if(!in_array(self::IGNORE_UNIQUE, $args)) {
-			$results['unique_input'] = $this->parse_unique_input();
-			$counters['unique_input'] = array_sum($results['unique_input']);
+		if(!in_array(self::UNIQUEINPUT, $args)) {
+			$results[self::UNIQUEINPUT] = $this->parse_unique_input();
+			$counters[self::UNIQUEINPUT] = array_sum($results['unique_input']);
 		}
 
 
-
-		// Now we run the readonly removal.
-		if(!in_array(self::IGNORE_READONLY, $args)) {
-			$counters['readonly'] = $this->replace_readonly();
+		// Now we run the readonly reformat.
+		if(!in_array(self::READONLY, $args)) {
+			$counters[self::READONLY] = $this->replace_readonly();
 		}
+
+		if(!in_array(self::EMPTYTAGS, $args)) {
+			// Add in the counter for the empty tags.
+			$counters[self::EMPTYTAGS] = 0;
+			$this->doc_object->set_callback(function($element) use (&$counters) {
+				// This will input html comments in to any element that is "empty".
+				if(empty($element->innertext)) {
+					$element->innertext = '<!-- -->';
+					$counters[self::EMPTYTAGS]++;
+				}
+			});
+		}
+
+
 	}
 }
