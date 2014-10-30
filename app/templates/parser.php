5075892302da0af6e60786380bc1bd66155ea95d
Duane Jeffers <duane@duanejeffers.com>
2014-10-30 16:29:08 -0500

    Parser Template finished

--- app/templates/parser.php
+++ app/templates/parser.php
@@ -12,14 +12,57 @@ use TJC\Parser\HTML as HTMLParser;
 		</div>
 	</div>
 	<?php endif; ?>
-	<?php foreach($data['results'] as $key => $val): ?>
 	<div class="row">
-		<div class="col-md-12">
-			<?= display_stats($key, $val); ?>
+		<div class="col-md-12 page-header">
+			<h3>Parser Results</h3>
 		</div>
 	</div>
-	<?php endforeach; ?>
-	<?php // Download the files ...
-		
-	?>
+	<div class="row">
+		<div class="col-md-12">
+			<table class="table table-striped table-condensed">
+				<colgroup>
+					<col class="col-md-1">
+					<col class="col-md-11">
+				</colgroup>
+				<thead>
+					<tr>
+						<th><span class="glyphicon glyphicon-ok"></span></th>
+						<th>Details</th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php foreach($data['results'] as $key => $val): ?>
+					<?= display_stats($key, $val); ?>
+				<?php endforeach; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 page-header">
+			<h3>Download Parsed HTML</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<table class="table table-condensed table-striped">
+				<colgroup>
+					<col class="col-md-2">
+					<col class="col-md-10">
+				</colgroup>
+				<thead>
+					<tr>
+						<th>Download Link</th>
+						<th>Filename</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?= display_download($data['source']->id); ?>
+					<?php foreach($data['sources'] as $id => $source): ?>
+						<?= display_download($source->source_id); ?>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
