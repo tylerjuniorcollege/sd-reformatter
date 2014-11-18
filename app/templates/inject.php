eb47b35da26cc8f5a2e6b582db76c3c1e81f50a8
Duane Jeffers <duane@duanejeffers.com>
2014-11-18 17:00:16 -0600

    Inject page updated to include new layout code.

--- app/templates/inject.php
+++ app/templates/inject.php
@@ -42,8 +42,29 @@
 				</div>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-md-1 col-md-offset-11">
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</div>
+		</div>
 	</form>
 </div>
+<?php
+	$app = \Slim\Slim::getInstance();
+
+$editorCode = <<<JS
+	var jsEditor = ace.edit("jseditor");
+		jsEditor.setTheme("ace/theme/dreamweaver");
+		jsEditor.getSession().setMode("ace/mode/javascript");
+
+	var cssEditor = ace.edit("csseditor");
+		cssEditor.setTheme("ace/theme/dreamweaver");
+		cssEditor.getSession().setMode("ace/mode/css");
+JS;
+
+	$app->view->appendJavascriptFile('//cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/ace.js')
+			  ->appendJavascript($editorCode);
+/*
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/ace.js"></script>
 <script type="text/javascript">
 	var jsEditor = ace.edit("jseditor");
@@ -54,3 +75,4 @@
 		cssEditor.setTheme("ace/theme/dreamweaver");
 		cssEditor.getSession().setMode("ace/mode/css");
 </script>
+*/ ?>
